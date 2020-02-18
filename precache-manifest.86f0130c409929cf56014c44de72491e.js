fd4961d5512955bade8c3cfa3d750c51c1358b3e
Carlos <mlvenezuelan@gmail.com>
2020-02-18 14:12:57 -0400

    Updates

---
+++ precache-manifest.86f0130c409929cf56014c44de72491e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f31951108644c81313f1fae85b7505a",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "96ccbb78c808620fe244",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fa79756b9696420ba29e",
+    "url": "/facerecognition/static/css/main.faf9e171.chunk.css"
+  },
+  {
+    "revision": "96ccbb78c808620fe244",
+    "url": "/facerecognition/static/js/2.5bf74e40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerecognition/static/js/2.5bf74e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa79756b9696420ba29e",
+    "url": "/facerecognition/static/js/main.1fdc207e.chunk.js"
+  },
+  {
+    "revision": "31b2659c121fcf3b619e",
+    "url": "/facerecognition/static/js/runtime-main.1b7659d3.js"
+  },
+  {
+    "revision": "4551b0d3862e719fe4771258a2800157",
+    "url": "/facerecognition/static/media/ai.4551b0d3.png"
+  }
+]);
